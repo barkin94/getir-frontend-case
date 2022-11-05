80295a2b260c284d5ce28a09d534caaca349949b
barkin <barkinsagin@gmail.com>
2022-11-05 17:15:35 +0300

    build: add colors, fonts and height to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-light': '#1EA4CE',
+        'blue-dark': '#147594',
+        'lighter-grey': '#FAFAFA',
+        'light-grey': '#E5E5E5',
+        'dark-grey': '#6F6F6F',
+        'blueish-dark-grey': '#697488'
+      },
+      fontSize: {
+        '13px': '13px',
+        '14px': '14px',
+        '20px': '20px'
+      },
+      height: {
+        '40px': '40px'
+      }
+    },
+
   },
   plugins: [],
 }
