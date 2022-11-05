0672b9be12253b4fbd018fb2e2ac79b41bd43121
barkin <barkinsagin@gmail.com>
2022-11-05 15:03:44 +0300

    add item/company models under core folder

---
+++ src/core/models/item.ts
@@ -0,0 +1,10 @@
+export interface Item {
+  tags: string[];
+  price: number;
+  name: string;
+  description: string;
+  slug: string;
+  added: number;
+  manufacturer: number;
+  itemType: string;
+}
