37b11cfc3fa7e8b0d8d8e37f6fd666846bf53d19
barkin <barkinsagin@gmail.com>
2022-11-05 14:57:35 +0300

    feat: add Filter component

---
+++ src/components/shared/Filter.tsx/Filter.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { CheckBox } from "../CheckBox";
+import { Input } from "../Input";
+
+export function Filter<DataType>({ data, filterField }: FilterProps<DataType>) {
+	const [items, setItems] = useState<FilterItem[]>([]);
+	
+	useEffect(() => {
+		const fieldNameToCountMap: Record<string, number> = {}
+		
+		data.forEach(item => {
+			if (!fieldNameToCountMap[item[filterField] as string])
+				fieldNameToCountMap[item[filterField] as string] = 0;
+			
+			fieldNameToCountMap[item[filterField] as string]++
+		})
+
+		if (Object.keys(fieldNameToCountMap).length === 0)
+			return;
+		const filterItems: FilterItem[] = [];
+		
+		Object.keys(fieldNameToCountMap).forEach((fieldName) => {
+			filterItems.push({
+				checked: false,
+				name: fieldName,
+				count: fieldNameToCountMap[fieldName],
+			});
+		});
+
+		filterItems.sort((a, b) => b.count - a.count)
+		filterItems.unshift({ checked: false, name: 'All', count: data.length })
+
+		setItems(filterItems);
+	}, [data, filterField])
+
+	const handleCheckClick = (index: number) => {
+		items[index] = { ...items[index], checked: !items[index].checked };
+		setItems(items);
+	}
+
+	return (
+		<>
+			<Input />
+			{items.map(({ checked, name, count }, index) => (
+				<div key={index}>
+					<CheckBox
+						checked={checked}
+						label={`${name}`}
+						onClick={() => handleCheckClick(index)}
+					/>
+					<span className="">{`(${count})`}</span>
+				</div>
+			))}
+		</>
+	);
+}
+
+export interface FilterProps<DataType> {
+	data: DataType[]
+	filterField: (keyof DataType)
+}
+
+interface FilterItem {
+	checked: boolean;
+	name: string | JSX.Element;
+	count: number;
+}
