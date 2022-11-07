e2369795cfd0ae0cfe973652df262686bd7255e4
barkin <barkinsagin@gmail.com>
2022-11-07 22:56:03 +0300

    refactor: html tag semantic rename

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -3,13 +3,13 @@ import { SortAndFiltersSection } from "./SortAndFiltersSection/SortAndFiltersSec
 
 export function Content() {
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 my-8 mx-16">
-			<div className="md:col-span-1 lg:col-span-1">
+		<section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 my-8 mx-16">
+			<section className="md:col-span-1 lg:col-span-1">
 				<SortAndFiltersSection />
-			</div>
-			<div className="md:col-span-2 lg:col-span-3">
+			</section>
+			<section className="md:col-span-2 lg:col-span-3">
 				<ProductsSection />
-			</div>
-		</div>
+			</section>
+		</section>
 	);
 }
--- src/components/Content/SortAndFiltersSection/SortAndFiltersSection.tsx
+++ src/components/Content/SortAndFiltersSection/SortAndFiltersSection.tsx
@@ -5,16 +5,16 @@ import { Tags } from "./Tags";
 
 export function SortAndFiltersSection() {
 	return (
-		<section>
-			<div className="mb-5">
+		<>
+			<section className="mb-5">
 				<Sorting />
-			</div>
-			<div className="mb-5">
+			</section>
+			<section className="mb-5">
 				<Brands />
-			</div>
-			<div className="mb-5">
+			</section>
+			<section className="mb-5">
 				<Tags />
-			</div>
-		</section>
+			</section>
+		</>
 	);
 }
